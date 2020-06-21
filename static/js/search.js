1b8b952b6bb99f0a9dc19ead0b26bdacec72276b
Dvesti <dvesti@hot.ee>
2020-06-21 18:49:17 +0300

    my message

--- static/js/search.js
+++ static/js/search.js
@@ -1,137 +1,231 @@
-var normalizer = document.createElement("textarea");
-var normalize = function (input) {
-    normalizer.innerHTML = input;
-    var inputDecoded = normalizer.value;
-    return " " + inputDecoded.trim().toLowerCase()
-        .replace(/[^0-9a-z ]/gi, " ").replace(/\s+/g, " ") + " ";
-}   
 
-$("#searchBox").hide();
-var searchHost = {};
-$.getJSON("/index.json", function (results) {
-    searchHost.index = [];
-    var dup = {};
-    results.forEach(function (result) {
-        if (result.tags && !dup[result.permalink]) {
-            var res = {};
-            res.showTitle = result.title;
-            res.showDescription = result.description;
-            res.title = normalize(result.title);
-            res.subtitle = normalize(result.subtitle);
-            res.description = normalize(result.description);
-            res.content = normalize(result.content);
-            var newTags_1 = [];
-            result.tags.forEach(function (tag) {
-                return newTags_1.push(normalize(tag));
-            });
-            res.tags = newTags_1;
-            res.permalink = result.permalink;
-            res.image = result.image;
-            searchHost.index.push(res);
-            dup[result.permalink] = true;
-        }
-    });
-    $("#loading").hide();
-    $("#searchBox").show()
-        .removeAttr("disabled")
-        .focus();
-    initSearch();
-});
 
-var initSearch = function () {
-   $("#searchBox").keyup(function () {
-       runSearch();
-   });
+
+
+
+6++var summaryInclude = 60;
+var fuseOptions = {
+    shouldSort: true,
+    includeMatches: true,
+    threshold: 0.0,
+    tokenize: true,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: [
+        { name: "title", weight: 0.8 },
+        { name: "contents", weight: 0.5 },
+        { name: "tags", weight: 0.3 },
+        { name: "categories", weight: 0.3 }
+    ]
 };
+var searchQuery;
+var startTime;
+window.addEventListener("popstate", init);
+window.addEventListener("load", init);
 
-var runSearch = function () {
-   if (searching) {
-       return;
-   }
-   var term = normalize($("#searchBox").val()).trim();
-   if (term.length < minChars) {
-       $("#results").html('<p>No items found.</p>');
-       return;
-   }
-   searching = true;
-   $("#results").html('<p>Processing search...</p>');
-   var terms = term.split(" ");
-   var termsTree = [];
-   for (var i = 0; i < terms.length; i += 1) {
-       for (var j = i; j < terms.length; j += 1) {
-           var weight = Math.pow(2, j - i);
-           var str = "";
-           for (var k = i; k <= j; k += 1) {
-               str += (terms[k] + " ");
-           }
-           var newTerm = str.trim();
-           if (newTerm.length >= minChars && stopwords.indexOf(newTerm) < 0) {
-               termsTree.push({
-                   weight: weight,
-                   term: " " + str.trim() + " "
-               });
-           }
-       }
-   }
-   search(termsTree);
-   searching = false;
-};
-
-var search = function (terms) {
-   var results = [];
-   searchHost.index.forEach(function (item) {
-       if (item.tags) {
-           var weight_1 = 0;
-           terms.forEach(function (term) {
-               if (item.title.startsWith(term.term)) {
-                   weight_1 += term.weight * 32;
-               }
-           });
-           weight_1 += checkTerms(terms, 1, item.content);
-           weight_1 += checkTerms(terms, 2, item.description);
-           weight_1 += checkTerms(terms, 2, item.subtitle);
-           item.tags.forEach(function (tag) {
-               weight_1 += checkTerms(terms, 4, tag);
-           });
-           weight_1 += checkTerms(terms, 16, item.title);
-           if (weight_1) {
-               results.push({
-                   weight: weight_1,
-                   item: item
-               });
-           }
-       }
-   });
+function init() {
+    searchQuery = param("s");
+    if (searchQuery) {
+        $("#search-query").val(searchQuery);
+        executeSearch(searchQuery);
+    }
 }
 
-var checkTerms = function (terms, weight, target) {
-   var weightResult = 0;
-   terms.forEach(function (term) {
-       if (~target.indexOf(term.term)) {
-           var idx = target.indexOf(term.term);
-           while (~idx) {
-               weightResult += term.weight * weight;
-               idx = target.indexOf(term.term, idx + 1);
-           }
-       }
-   });
-   return weightResult;
-};
+var executeInlineSearch = debounce(
+    function() {
+        $(".my-auto .loading-skeleton").show();
+        $(".search-results-empty").remove();
+        $(".search-results-summary").remove();
+        $("#search-results .left").text("");
+        var query = document.getElementById("search-query").value.trim();
+        // write current query to address bar!
+        window.history.pushState(null, null, "?s=" + query);
+        if (query !== "") {
+            searchQuery = query;
+            executeSearch(query);
+        } else {
+            $(".my-auto .loading-skeleton").hide();
+            $("#search-results").append(
+                '<p class="search-results-empty">输入为空哦( ˘•ω•˘ )</p>'
+            );
+        }
+    },
+    300,
+    false
+);
 
-var render = function (results) {
-   results.sort(function (a, b) { return b.weight - a.weight; });
-   for (var i = 0; i < results.length && i < limit; i += 1) {
-       var result = results[i].item;
-       var resultPane = "<div class=\"container\">" +
-           ("<div class=\"row\"><a href=\"" + result.permalink + "\" ") +
-           ("alt=\"" + result.showTitle + "\">" + result.showTitle + "</a>" +
-               "</div>") +
-           "<div class=\"row\"><div class=\"float-left col-2\">" +
-           ("<img src=\"" + result.image + "\" alt=\"" + result.showTitle + "\" class=\"rounded img-thumbnail\">") +
-           "</div>" +
-           ("<div class=\"col-10\"><small>" + result.showDescription + "</small></div>") +
-           "</div></div>";
-       $("#results").append(resultPane);
-   }
-};
+function executeSearch(searchQuery) {
+    startTime = window.performance.now();
+    $.getJSON("/index.json", function(data) {
+        var pages = data;
+        var fuse = new Fuse(pages, fuseOptions);
+        var result = fuse.search(searchQuery);
+        if (result.length > 0) {
+            populateResults(result);
+        } else {
+            var duration = parseInt(window.performance.now() - startTime);
+            $("#search-results .left").text(
+                "用时：" + duration + "ms  共找到" + result.length + "条结果"
+            );
+            $("#search-results").append(
+                '<p class="search-results-empty">抱歉没找到X﹏X</p>'
+            );
+            $(".my-auto .loading-skeleton").hide();
+        }
+    });
+}
 
+function populateResults(result) {
+    $.each(result, function(key, value) {
+        var contents = value.item.contents;
+        var snippet = "";
+        var snippetHighlights = [];
+        var tags = [];
+        if (fuseOptions.tokenize) {
+            snippetHighlights.push(searchQuery);
+        } else {
+            $.each(value.matches, function(matchKey, mvalue) {
+                if (mvalue.key == "tags" || mvalue.key == "categories") {
+                    snippetHighlights.push(mvalue.value);
+                } else if (mvalue.key == "contents") {
+                    start =
+                        mvalue.indices[0][0] - summaryInclude > 0 ?
+                        mvalue.indices[0][0] - summaryInclude :
+                        0;
+                    end =
+                        mvalue.indices[0][1] + summaryInclude < contents.length ?
+                        mvalue.indices[0][1] + summaryInclude :
+                        contents.length;
+                    snippet += contents.substring(start, end);
+                    snippetHighlights.push(
+                        mvalue.value.substring(
+                            mvalue.indices[0][0],
+                            mvalue.indices[0][1] - mvalue.indices[0][0] + 1
+                        )
+                    );
+                }
+            });
+        }
+
+        if (snippet.length < 1) {
+            snippet += contents.substring(0, summaryInclude * 2);
+        }
+        //pull template from hugo templarte definition
+        var templateDefinition = $("#search-result-template").html();
+        //replace values
+        var output = render(templateDefinition, {
+            key: key,
+            title: value.item.title,
+            link: value.item.permalink,
+            tags: value.item.tags,
+            categories: value.item.categories,
+            date: dateFormat(value.item.date),
+            snippet: snippet
+        });
+        $("#search-results").append(output);
+
+        $.each(snippetHighlights, function(snipkey, snipvalue) {
+            $("#summary-" + key).mark(snipvalue);
+        });
+    });
+
+    var duration = parseInt(window.performance.now() - startTime);
+    $("#search-results .left").text(
+        "用时：" + duration + "ms  共找到" + result.length + "条结果"
+    );
+    $(".my-auto .loading-skeleton").hide();
+}
+
+function param(name) {
+    return decodeURIComponent(
+        (location.search.split(name + "=")[1] || "").split("&")[0]
+    ).replace(/\+/g, " ");
+}
+
+function dateFormat(time) {
+    let date = new Date(time * 1000),
+        y = date.getFullYear(),
+        m = date.getMonth() + 1,
+        d = date.getDate(),
+        p = function(m) {
+            return m <= 9 ? "0" + m : m;
+        };
+
+    return y + "-" + p(m) + "-" + p(d);
+}
+
+function render(templateString, data) {
+    var conditionalMatches, conditionalPattern, copy;
+    conditionalPattern = /\$\{\s*isset ([a-zA-Z]*) \s*\}(.*)\$\{\s*end\s*}/g;
+    //since loop below depends on re.lastInxdex, we use a copy to capture any manipulations whilst inside the loop
+    copy = templateString;
+    while (
+        (conditionalMatches = conditionalPattern.exec(templateString)) !== null
+    ) {
+        if (data[conditionalMatches[1]]) {
+            //valid key, remove conditionals, leave contents.
+            copy = copy.replace(conditionalMatches[0], conditionalMatches[2]);
+        } else {
+            //not valid, remove entire section
+            copy = copy.replace(conditionalMatches[0], "");
+        }
+    }
+    templateString = copy;
+    //now any conditionals removed we can do simple substitution
+    var key, find, re;
+    for (key in data) {
+        find = "\\$\\{\\s*" + key + "\\s*\\}";
+        re = new RegExp(find, "g");
+        templateString = templateString.replace(re, data[key]);
+    }
+    return templateString;
+}
+
+// Credit David Walsh (https://davidwalsh.name/javascript-debounce-function)
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+    var timeout;
+
+    // This is the function that is actually executed when
+    // the DOM event is triggered.
+    return function executedFunction() {
+        // Store the context of this and any
+        // parameters passed to executedFunction
+        var context = this;
+        var args = arguments;
+
+        // The function to be called after
+        // the debounce time has elapsed
+        var later = function() {
+            // null timeout to indicate the debounce ended
+            timeout = null;
+
+            // Call function now if you did not on the leading end
+            if (!immediate) func.apply(context, args);
+        };
+
+        // Determine if you should call the function
+        // on the leading or trail end
+        var callNow = immediate && !timeout;
+
+        // This will reset the waiting every function execution.
+        // This is the step that prevents the function from
+        // being executed because it will never reach the
+        // inside of the previous setTimeout
+        clearTimeout(timeout);
+
+        // Restart the debounce waiting period.
+        // setTimeout returns a truthy value (it differs in web vs node)
+        timeout = setTimeout(later, wait);
+
+        // Call immediately if you're dong a leading
+        // end execution
+        if (callNow) func.apply(context, args);
+    };
+}
