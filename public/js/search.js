1f84f1cec6ef9848e729189f50fb74efd479509c
Dvesti <dvesti@hot.ee>
2020-08-07 00:20:20 +0300

    my message

--- public/js/search.js
+++ public/js/search.js
@@ -26,7 +26,7 @@ if (searchQuery) {
 
 
 function executeSearch(searchQuery) {
-    fetch("/index.json")
+    fetch("/1/index00.json")
         .then(response => response.json())
         .then(data => {
             data = data.map(d => {
